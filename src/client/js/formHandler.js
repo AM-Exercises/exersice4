b1fefe6ed21c221077af588553f3592619de4701
AM-Exercises <72548237+AM-Exercises@users.noreply.github.com>
2020-10-24 10:21:47 +0300

    Add files via upload

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,18 +2,19 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
 
-    Client.checkForName(formText)
-
+    let formText = document.getElementById('sentence').value;
+    
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+
+    fetch('/sentiment',{ fText: formText })
     .then(res => {
         return res.json()
     })
     .then(function(data) {
         document.getElementById('results').innerHTML = data.message
     })
+    
 }
 
 export { handleSubmit }
